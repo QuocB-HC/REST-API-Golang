d41f9a9f9ed768fb20866f3e5331c5fa438655f5
BaoNVQ2903 <nguyenvoquocbao292003@gmail.com>
2025-04-19 18:08:04 +0700

    fix model again

--- models/todo_item.model.go
+++ models/todo_item.model.go
@@ -1,33 +1,38 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/datatypes"
+)
 
 type TodoItem struct {
-	ID          string     `json:"id" gorm:"column:id"`
-	Title       string     `json:"title" gorm:"column:title"`
-	Image       string     `json:"image" gorm:"column:image"`
-	Description string     `json:"description" gorm:"column:description"`
-	Status      string     `json:"status" gorm:"column:status"`
-	CreatedAt   *time.Time `json:"created_at" gorm:"column:created_at"`
-	UpdatedAt   *time.Time `json:"updated_at" gorm:"column:updated_at"`
+	ID          uuid.UUID      `json:"id" gorm:"column:id;type:uuid;default:uuid_generate_v4();primaryKey"`
+	Title       string         `json:"title" gorm:"column:title;type:varchar(100);not null"`
+	Image       datatypes.JSON `json:"image" gorm:"column:image"`
+	Description string         `json:"description" gorm:"column:description"`
+	Status      string         `json:"status" gorm:"column:status;type:status_enum;default:'Doing';not null"`
+	CreatedAt   time.Time      `json:"created_at" gorm:"column:created_at;type:timestamp"`
+	UpdatedAt   time.Time      `json:"updated_at" gorm:"column:updated_at;type:timestamp"`
 }
 
 func (TodoItem) TableName() string { return "todo_items" }
 
 type TodoItemCreation struct {
 	Title       string `json:"title" binding:"required" gorm:"column:title"`
-	Image       string `json:"image" gorm:"column:image"`
 	Description string `json:"description" gorm:"column:description"`
-	// Status      string     `json:"status" gorm:"column:status;type:status_enum;default:'Doing'"`
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
 }
 
 func (TodoItemCreation) TableName() string { return TodoItem{}.TableName() }
 
 type TodoItemUpdate struct {
 	Title       *string `json:"title" gorm:"column:title"`
-	Image       string  `json:"image" gorm:"column:image"`
 	Description *string `json:"description" gorm:"column:description"`
 	Status      *string `json:"status" gorm:"column:status"`
+	UpdatedAt   time.Time
 }
 
 func (TodoItemUpdate) TableName() string { return TodoItem{}.TableName() }
